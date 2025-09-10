07bad1674d4723547157f29fbd859f46dc4969a8
dooohun <ehgns0305@naver.com>
2025-09-10 20:34:42 +0900

    fix: page router로 변경 및 초기 세팅 변경

--- src/app/page.tsx
+++
@@ -1,7 +0,0 @@
-import PRCommitCounter from "@/components/PRCommitCounter";
-
-export default function Home() {
-  return (
-    <PRCommitCounter />
-  )
-}
--- src/components/PRCommitCounter.tsx
+++
@@ -1,7 +0,0 @@
-export default function PRCounter() {
-  return (
-    <div>
-      <button>버튼이요</button>
-    </div>
-  )
-}
